ea4b73b238901565d26a84c93fed3d2c20170219
snowhyt <oliverglorioso.44@gmail.com>
2025-02-03 20:46:33 +0800

    first bleach ako

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connecToMongoDB.js";
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -14,5 +15,6 @@ app.use("/api/auth",authRoutes);
 
 
 app.listen(PORT, () =>{
-    console.log(`Server started on port ${PORT}`);
+    connectToMongoDB();
+    console.log(`Server is running on port ${PORT}`);
 });
